e71f75bb41277eacea4fef3651be6319dcab0371
Ujala-355 <ujalasaini355@gmail.com>
2024-02-03 15:21:17 +0530

    title test done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>First React Test case</h1>
+        <img title="12 img papi 5675" src="https://img.freepik.com/free-photo/puppy-that-is-walking-snow_1340-37228.jpg" alt="img 1"/>
     </div>
   );
 }
 
 export default App;
+
+// import React,{useState,useMemo} from "react";
+// const Memo=()=>{
+//     const [one,setOne]=useState(0);
+//     const [two,setTwo]=useState(0);
+
+//     function increment(){
+//         setOne(one+1);
+//     }
+//     function decrement(){
+//         setTwo(two-1);
+//     }
+//     const isEven=useMemo(()=>{
+//       for(let i=0; i<10000000000; i++){
+//           return one%2===0;
+//       }
+//     },[one])
+//     return(
+//         <>
+//             <button onClick={increment}>Count one {one}</button>
+//             <button onClick={decrement}>Count Two {two}</button>
+//         </>
+//     )
+// }
+// export default Memo;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,25 @@
-import { render, screen } from '@testing-library/react';
+// import { render, screen } from '@testing-library/react';
+// import App from './App';
+
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+import {render,screen} from "@testing-library/react";
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test("First React Test case",()=>{
+    render(<App/>);
+    const text=screen.getByText("First React Test case");
+    expect (text).toBeInTheDocument();
+})
+
+test (" hello First React Test case",()=>{
+    render(<App/>);
+    const text=screen.getByText("First React Test case");
+    const title=screen.getByTitle(/img papi/i)
+    expect (text).toBeInTheDocument();
+    expect (title).toBeInTheDocument()
+})
