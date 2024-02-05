d2d59d58c24825291d66a106cd890f3ec38afc62
Ujala-355 <ujalasaini355@gmail.com>
2024-02-05 14:28:13 +0530

    test to change event

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 
 import './App.css';
-
+import {useState} from "react";
 function App() {
+    const [data,setData]=useState("");
+
   return (
     <div className="App">
         <h1>First React Test case</h1>
-        <input type="text" placeholder="enter your papi name" name="papi" id="papiId"/>
-        <br/>
         <br/>
         <img title="12 img papi 5675"  src="https://img.freepik.com/free-photo/puppy-that-is-walking-snow_1340-37228.jpg" alt="img 1"/>
+        <input type="text" value={data} onChange={(e)=>setData(e.target.value+"test")}/>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -7,29 +7,43 @@
 //   expect(linkElement).toBeInTheDocument();
 // });
 
-import {render,screen} from "@testing-library/react";
-import App from './App';
+// import {render,screen} from "@testing-library/react";
+// import App from './App';
 
-test("First React Test case",()=>{
-    render(<App/>);
-    const text=screen.getByText("First React Test case");
-    expect (text).toBeInTheDocument();
-})
+// test("First React Test case",()=>{
+//     render(<App/>);
+//     const text=screen.getByText("First React Test case");
+//     expect (text).toBeInTheDocument();
+// })
 
-test (" hello First React Test case",()=>{
-    render(<App/>);
-    const text=screen.getByText("First React Test case");
-    const title=screen.getByTitle(/img papi/i)
-    expect (text).toBeInTheDocument();
-    expect (title).toBeInTheDocument()
-})
+// test (" hello First React Test case",()=>{
+//     render(<App/>);
+//     const text=screen.getByText("First React Test case");
+//     const title=screen.getByTitle(/img papi/i)//(/text/i) yh use karne se yh upper lower case check nahi karta hai tabi tast case pass hoga
+//     expect (text).toBeInTheDocument();
+//     expect (title).toBeInTheDocument()
+// })
 
-test("Input test case",()=>{
-    render (<App/>)
-    let checkInput=screen.getByRole("textbox");// textBox is predefine
-    let checkPlaceholder=screen.getByPlaceholderText("enter your papi name");
-    expect (checkInput).toBeInTheDocument();
-    expect (checkPlaceholder).toBeInTheDocument();
-    expect (checkInput).toHaveAttribute("name","papi");
-    expect (checkInput).toHaveAttribute("id","papiId")
+// test("Input test case",()=>{
+//     render (<App/>)
+//     let checkInput=screen.getByRole("textbox");// textBox is predefine
+//     let checkPlaceholder=screen.getByPlaceholderText("enter your papi name");
+//     expect (checkInput).toBeInTheDocument();
+//     expect (checkPlaceholder).toBeInTheDocument();
+//     expect (checkInput).toHaveAttribute("name","papi");
+//     expect (checkInput).toHaveAttribute("id","papiId")
+// });
+
+
+// make input box in the component
+// Define state and use with on change event
+// import component in test file
+// Write code for test case
+import {fireEvent,render,screen} from "@testing-library/react";
+import App from "./App";
+test("on change event testing 1",()=>{
+    render(<App/>)
+    let input=screen.getByRole("textbox");
+    fireEvent.change(input,{target:{value:"a"}});
+    expect(input.value).toBe("atest")
 });
