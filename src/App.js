e340c2c662278b9713dca822482c68174a55af34
Ujala-355 <ujalasaini355@gmail.com>
2024-02-11 16:50:40 +0530

    Multiple elements with Role Custom Role

--- src/App.js
+++ src/App.js
@@ -2,8 +2,14 @@ const App=()=>{
     return(
         <>
             <div>
-                  <h1>getByRole</h1>
-                  <input type="text" defaultValue="hello" disabled/>
+                  <h1>Multiple Item with Role</h1>
+                  <h1>Custom Role</h1>
+                  <button>Click1</button>
+                  <button>Click2</button>
+                  <label htmlFor="input1">input 1</label>
+                  <input type="text" id="input1" />
+                  <label htmlFor="input2">input 2</label>
+                  <input type="text" id="input2" />
             </div>
         </>
     )
--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,15 @@
 import {render,screen} from "@testing-library/react";
 import App from "./App";
 
-test("get by role",()=>{
-    render(<App/>);
-    const inputField=screen.getByRole("textbox");
-    expect(inputField).toBeInTheDocument();
-    expect(inputField).toHaveValue("hello")
-    expect(inputField).toBeDisabled();
+test("getByRole testing in custom",()=>{
+    render(<App/>)
+    const btn1=screen.getByRole("button",{name:"Click1"});
+    const btn2=screen.getByRole("button",{name:"Click2"});
+    const input1=screen.getByRole("textbox",{name:"input 1"});
+    const input2=screen.getByRole("textbox",{name:"input 2"})
+
+    expect(input1).toBeInTheDocument();
+    expect(input1).toBeInTheDocument();
+    expect(btn1).toBeInTheDocument();
+    expect(btn2).toBeInTheDocument();
 })
