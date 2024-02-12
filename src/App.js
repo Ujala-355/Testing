4dcd28db0ee14c84dcd1a1fdbba34ed04bdb0ca5
Ujala-355 <ujalasaini355@gmail.com>
2024-02-12 14:10:10 +0530

    gwtByPlaceholderText

--- src/App.js
+++ src/App.js
@@ -2,22 +2,8 @@ const App=()=>{
     return(
         <>
             <div>
-                <h1>RTL Query: getByLabelText multiple elements</h1>
-                <label htmlFor="CheckBox">Skills</label>
-                <input type="text" id="CheckBox" defaultValue={"ujala"}/>
-                <br/>
-
-                <label htmlFor="CheckBox1">Skills</label>
-                <input type="text" id="CheckBox1" defaultValue={"ujala"}/>
-                <br/>
-                <label htmlFor="CheckBox2">Skills</label>
-                <input type="text" id="CheckBox2" defaultValue={"ujala"}/>
-                <br/>
-                <label htmlFor="CheckBox3">Skills</label>
-                <input type="text" id="CheckBox3" defaultValue={"ujala"}/>
-                <br/>
-                <label htmlFor="CheckBox4">Skills</label>
-                <input type="text" id="CheckBox4" defaultValue={"ujala"}/>
+                <h1>RTL Query: ByPlaceholderText</h1>
+                <input type="text" placeholder="enter username"/>
             </div> 
         </>
     )
--- src/App.test.js
+++ src/App.test.js
@@ -2,11 +2,8 @@ import { render, screen } from "@testing-library/react";
 import App from "./App";
 
 test("getLabelText multiple", () => {
-  render(<App />);
-    const inputs=screen.getAllByLabelText("Skills");
-    // expect(inputs[3]).toBeInTheDocument();
-    for (let i=0; i<inputs.length;i++){
-        expect(inputs[i]).toBeInTheDocument();
-        expect(inputs[i]).toHaveValue("ujala");
-    }
+    render(<App />);
+    const inputs=screen.getByPlaceholderText("enter username");
+    expect(inputs).toBeInTheDocument();
+ 
 });
