28bec9f84988578f2211abd95cf99964ca94933a
Ujala-355 <ujalasaini355@gmail.com>
2024-02-14 16:41:26 +0530

    getByTestId

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
 const App=()=>{
     return(
         <>
-            <div>
-                <h1>RTL Query: getByText and getAllByText</h1>
-                <button>Login</button>
-                <h1>H1 Tag testing</h1>  
-                <p className="Ptext">P tag testing</p>
-                <h1>H1 Tag testing</h1>
-                <h1>H1 Tag testing</h1>
+            <div data-testid="div-test-id">
+                <h1>RTL Query: getByTestId</h1>
                 <h1>H1 Tag testing</h1> 
             </div> 
         </>
--- src/App.test.js
+++ src/App.test.js
@@ -1,23 +1,8 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("single button testing",()=>{
+test("hello",()=>{
     render(<App/>)
-    const btn=screen.getByText("Login");
-    expect(btn).toBeInTheDocument();
-})
-test("single p testing",()=>{
-    render(<App/>)
-    const btn=screen.getByText("P tag testing");
-    expect(btn).toBeInTheDocument();
-    expect(btn).toHaveClass("Ptext")
-})
-
-test("multiple h1 tag testing",()=>{
-    render(<App/>)
-    const H=screen.getAllByText("H1 Tag testing")
-    // expect(H[0]).toBeInTheDocument();
-    for (let i=0; i<H.length; i++){
-        expect(H[i]).toBeInTheDocument();
-    }
+    const d=screen.getByTestId("div-test-id")
+    expect(d).toBeInTheDocument();
 })
