7d94547ff5199209147c52ea1272f0b26396bff0
Ujala-355 <ujalasaini355@gmail.com>
2024-02-18 00:12:17 +0530

    act function

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,12 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen ,act} from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import App from "./App";
 
 test("On change event testing",async ()=>{
-    userEvent.setup();
     render(<App/>);
     const el=screen.getByRole("textbox")
-    await userEvent.type(el,"ujala")
+    await act(async()=>{
+        await userEvent.type(el,"ujala")
+    })
     expect(screen.getByText("ujala")).toBeInTheDocument();
 })
